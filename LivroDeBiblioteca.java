2b8093693fe0e1e8def1054e64c577b0c69aa182
Leme-do-Prado <102317613+Leme-do-Prado@users.noreply.github.com>
2022-10-24 18:00:02 -0300

    Create LivroDeBiblioteca.java

---
+++ LivroDeBiblioteca.java
@@ -0,0 +1,39 @@
+public class LivroDeBiblioteca extends Livro implements ItemDeBiblioteca {
+	boolean emprestado = false;
+	String localizacao;
+	
+	public LivroDeBiblioteca() {
+		super();
+		this.localizacao = " ";
+	}
+	
+	public LivroDeBiblioteca(String t, String a, String e, short ae, String l) {
+		super(t, a, e, ae);
+		this.localizacao = l;
+	}
+	
+	public String getDados() {
+		return "Titulo = " + this.titulo + "Autor = " + this.autor + "Editora = "+ this.editora + "Ano = " +this.anoedicao + "Localização: " + this.localizacao;
+	}
+	
+	public void Empresta() {
+			this.emprestado = true;
+	}
+	
+	public void Devolve() {
+			this.emprestado = false;
+	}
+
+	public boolean getEmprestado() {
+		return this.emprestado;
+	}
+
+	public String getLocalizacao() {
+		return this.localizacao;
+
+	}
+	
+	public void setLocalizacao(String l) {
+		this.localizacao = l;
+	}
+}
